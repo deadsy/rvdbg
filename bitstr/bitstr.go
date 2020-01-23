28a96cefbc584fc9e1a8f7de7f1accd992951725
Jason Harris <sirmanlypowers@gmail.com>
2020-01-23 15:02:36 -0800

    Added drop head/tail.

--- bitstr/bitstr.go
+++ bitstr/bitstr.go
@@ -40,14 +40,16 @@ const setSize = 64
 const zeroes = uint64(0)
 const ones = uint64((1 << 64) - 1)
 
+// bitSet stores 0 to 64 bits.
 type bitSet struct {
 	val uint64 // bits in this set
 	n   uint   // number of bits in this set
 }
 
+// newBitSet returns a bit set of 0 to 64 bits.
 func newBitSet(val uint64, n uint) bitSet {
 	if n > setSize {
-		panic("n != setSize")
+		panic("n > setSize")
 	}
 	if n < 64 {
 		val &= uint64((1 << n) - 1)
@@ -56,6 +58,22 @@ func newBitSet(val uint64, n uint) bitSet {
 		val: val,
 		n:   n,
 	}
+}
+
+func (bs *bitSet) dropHead(n uint) {
+	if n > setSize {
+		panic("n > setSize")
+	}
+	bs.val >>= n
+	bs.n -= n
+}
+
+func (bs *bitSet) dropTail(n uint) {
+	if n > setSize {
+		panic("n > setSize")
+	}
+	bs.val &= uint64((1 << (bs.n - n)) - 1)
+	bs.n -= n
 }
 
 func (bs *bitSet) String() string {
@@ -68,11 +86,13 @@ func (bs *bitSet) String() string {
 
 //-----------------------------------------------------------------------------
 
+// BitString is a bit string of arbitrary length.
 type BitString struct {
 	set []bitSet // bit sets
 	n   uint
 }
 
+// NewBitString returns a new 0 length bitstring.
 func NewBitString() *BitString {
 	return &BitString{}
 }
@@ -99,6 +119,46 @@ func (b *BitString) Tail1(n uint) *BitString {
 	return b
 }
 
+// DropHead removes n bits from the head of the bit string.
+func (b *BitString) DropHead(n uint) *BitString {
+	if n >= b.n {
+		b.n = 0
+		b.set = []bitSet{}
+		return b
+	}
+	b.n -= n
+	for i := range b.set {
+		bs := &b.set[i]
+		l := min(n, bs.n)
+		bs.dropHead(l)
+		n -= l
+		if n == 0 {
+			break
+		}
+	}
+	return b
+}
+
+// DropTail removes n bits from the tail of the bit string.
+func (b *BitString) DropTail(n uint) *BitString {
+	if n >= b.n {
+		b.set = []bitSet{}
+		b.n = 0
+		return b
+	}
+	b.n -= n
+	for i := len(b.set) - 1; i >= 0; i-- {
+		bs := &b.set[i]
+		l := min(n, bs.n)
+		bs.dropTail(l)
+		n -= l
+		if n == 0 {
+			break
+		}
+	}
+	return b
+}
+
 func (b *BitString) String() string {
 	s := []string{}
 	for i := len(b.set) - 1; i >= 0; i-- {
@@ -112,17 +172,17 @@ func (b *BitString) String() string {
 
 //-----------------------------------------------------------------------------
 
-// Null return an empty bit string
+// Null returns an empty bit string.
 func Null() *BitString {
 	return NewBitString()
 }
 
-// Ones returns n one bits.
+// Ones returns a bit string with n-one bits.
 func Ones(n uint) *BitString {
 	return NewBitString().Tail1(n)
 }
 
-// Zeroes returns n zero bits
+// Zeroes returns a bit string with n-zero bits.
 func Zeroes(n uint) *BitString {
 	return NewBitString().Tail0(n)
 }
