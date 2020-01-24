01abe51778602d019f7e00c64417f584e1775721
Jason Harris <sirmanlypowers@gmail.com>
2020-01-23 17:03:19 -0800

    More bit string functions and tests.

--- bitstr/bitstr.go
+++ bitstr/bitstr.go
@@ -15,6 +15,11 @@ That is:
 
 [tail]...[body]...[head] <- Tx First
 
+The bits are stored in bitSets (up to 64 bits) and a slice of bitSets forms
+the bit string. The bitSets are stored in the slice head first. A head
+operation works on the start of the slice, while a tail operation works on
+the end of the slice.
+
 */
 //-----------------------------------------------------------------------------
 
@@ -22,12 +27,14 @@ package bitstr
 
 import (
 	"fmt"
+	"math/rand"
+	"strconv"
 	"strings"
 )
 
 //-----------------------------------------------------------------------------
 
-func min(a, b uint) uint {
+func min(a, b int) int {
 	if a < b {
 		return a
 	}
@@ -43,13 +50,13 @@ const ones = uint64((1 << 64) - 1)
 // bitSet stores 0 to 64 bits.
 type bitSet struct {
 	val uint64 // bits in this set
-	n   uint   // number of bits in this set
+	n   int    // number of bits in this set
 }
 
 // newBitSet returns a bit set of 0 to 64 bits.
-func newBitSet(val uint64, n uint) bitSet {
-	if n > setSize {
-		panic("n > setSize")
+func newBitSet(val uint64, n int) bitSet {
+	if n > setSize || n < 0 {
+		panic("")
 	}
 	if n < 64 {
 		val &= uint64((1 << n) - 1)
@@ -60,17 +67,17 @@ func newBitSet(val uint64, n uint) bitSet {
 	}
 }
 
-func (bs *bitSet) dropHead(n uint) {
-	if n > setSize {
-		panic("n > setSize")
+func (bs *bitSet) dropHead(n int) {
+	if n > setSize || n < 0 {
+		panic("")
 	}
 	bs.val >>= n
 	bs.n -= n
 }
 
-func (bs *bitSet) dropTail(n uint) {
-	if n > setSize {
-		panic("n > setSize")
+func (bs *bitSet) dropTail(n int) {
+	if n > setSize || n < 0 {
+		panic("")
 	}
 	bs.val &= uint64((1 << (bs.n - n)) - 1)
 	bs.n -= n
@@ -89,7 +96,7 @@ func (bs *bitSet) String() string {
 // BitString is a bit string of arbitrary length.
 type BitString struct {
 	set []bitSet // bit sets
-	n   uint
+	n   int
 }
 
 // NewBitString returns a new 0 length bitstring.
@@ -97,30 +104,35 @@ func NewBitString() *BitString {
 	return &BitString{}
 }
 
+// tail adds a bit set to the tail of the bit string.
+func (b *BitString) tail(bs bitSet) *BitString {
+	b.set = append(b.set, bs)
+	b.n += bs.n
+	return b
+}
+
 // Tail0 adds zero bits to the tail of the bit string.
-func (b *BitString) Tail0(n uint) *BitString {
+func (b *BitString) Tail0(n int) *BitString {
 	for n > 0 {
-		l := min(n, setSize)
-		b.set = append(b.set, newBitSet(zeroes, l))
-		n -= l
+		k := min(n, setSize)
+		b.tail(newBitSet(zeroes, k))
+		n -= k
 	}
-	b.n += n
 	return b
 }
 
 // Tail1 adds one bits to the tail of the bit string.
-func (b *BitString) Tail1(n uint) *BitString {
+func (b *BitString) Tail1(n int) *BitString {
 	for n > 0 {
-		l := min(n, setSize)
-		b.set = append(b.set, newBitSet(ones, l))
-		n -= l
+		k := min(n, setSize)
+		b.tail(newBitSet(ones, k))
+		n -= k
 	}
-	b.n += n
 	return b
 }
 
 // DropHead removes n bits from the head of the bit string.
-func (b *BitString) DropHead(n uint) *BitString {
+func (b *BitString) DropHead(n int) *BitString {
 	if n >= b.n {
 		b.n = 0
 		b.set = []bitSet{}
@@ -129,9 +141,9 @@ func (b *BitString) DropHead(n uint) *BitString {
 	b.n -= n
 	for i := range b.set {
 		bs := &b.set[i]
-		l := min(n, bs.n)
-		bs.dropHead(l)
-		n -= l
+		k := min(n, bs.n)
+		bs.dropHead(k)
+		n -= k
 		if n == 0 {
 			break
 		}
@@ -140,7 +152,7 @@ func (b *BitString) DropHead(n uint) *BitString {
 }
 
 // DropTail removes n bits from the tail of the bit string.
-func (b *BitString) DropTail(n uint) *BitString {
+func (b *BitString) DropTail(n int) *BitString {
 	if n >= b.n {
 		b.set = []bitSet{}
 		b.n = 0
@@ -149,9 +161,9 @@ func (b *BitString) DropTail(n uint) *BitString {
 	b.n -= n
 	for i := len(b.set) - 1; i >= 0; i-- {
 		bs := &b.set[i]
-		l := min(n, bs.n)
-		bs.dropTail(l)
-		n -= l
+		k := min(n, bs.n)
+		bs.dropTail(k)
+		n -= k
 		if n == 0 {
 			break
 		}
@@ -159,15 +171,38 @@ func (b *BitString) DropTail(n uint) *BitString {
 	return b
 }
 
-func (b *BitString) String() string {
+// Copy returns a new copy of the bit string.
+func (b *BitString) Copy() *BitString {
+	x := NewBitString()
+	for i := range b.set {
+		x.tail(b.set[i])
+	}
+	return x
+}
+
+// Head adds a bit string to the head of a bit string.
+func (b *BitString) Head(a *BitString) *BitString {
+	return nil
+}
+
+// Tail adds a bit string to the tail of a bit string.
+func (b *BitString) Tail(a *BitString) *BitString {
+	return nil
+}
+
+// BitString returns a 1/0 string for the bit string.
+func (b *BitString) BitString() string {
 	s := []string{}
 	for i := len(b.set) - 1; i >= 0; i-- {
-		ss := b.set[i].String()
-		if ss != "" {
-			s = append(s, ss)
+		if b.set[i].n > 0 {
+			s = append(s, b.set[i].String())
 		}
 	}
 	return strings.Join(s, "")
+}
+
+func (b *BitString) String() string {
+	return fmt.Sprintf("(%d) %s", b.n, b.BitString())
 }
 
 //-----------------------------------------------------------------------------
@@ -178,13 +213,42 @@ func Null() *BitString {
 }
 
 // Ones returns a bit string with n-one bits.
-func Ones(n uint) *BitString {
+func Ones(n int) *BitString {
 	return NewBitString().Tail1(n)
 }
 
 // Zeroes returns a bit string with n-zero bits.
-func Zeroes(n uint) *BitString {
+func Zeroes(n int) *BitString {
 	return NewBitString().Tail0(n)
+}
+
+// FromString returns a bit string from a 1/0 string.
+func FromString(s string) (*BitString, error) {
+	n := len(s)
+	b := NewBitString()
+	for n > 0 {
+		j := len(s)
+		k := min(n, setSize)
+		x, err := strconv.ParseUint(s[j-k:j], 2, 64)
+		if err != nil {
+			return nil, err
+		}
+		b.tail(newBitSet(x, k))
+		s = s[0 : j-k]
+		n -= k
+	}
+	return b, nil
+}
+
+// Random returns a random bit string of n bits.
+func Random(n int) *BitString {
+	b := NewBitString()
+	for n > 0 {
+		k := min(n, setSize)
+		b.tail(newBitSet(rand.Uint64(), k))
+		n -= k
+	}
+	return b
 }
 
 //-----------------------------------------------------------------------------
--- bitstr/bitstr_test.go
+++ bitstr/bitstr_test.go
@@ -26,35 +26,130 @@ func repeatRune(r rune, n int) string {
 
 func Test_BitString(t *testing.T) {
 
-	b0 := NewBitString().Tail0(2)
-	if b0.String() != "00" {
+	var a, b *BitString
+
+	b = NewBitString().Tail0(2)
+	if b.BitString() != "00" {
 		t.Error("FAIL")
 	}
 
-	b1 := NewBitString().Tail1(5)
-	if b1.String() != "11111" {
+	b = NewBitString().Tail1(5)
+	if b.BitString() != "11111" {
 		t.Error("FAIL")
 	}
 
-	b0 = b0.Tail1(3)
-	if b0.String() != "11100" {
+	b = NewBitString().Tail0(2).Tail1(3)
+	if b.BitString() != "11100" {
 		t.Error("FAIL")
 	}
 
-	b1 = b1.Tail0(7)
-	if b1.String() != "000000011111" {
+	b = NewBitString().Tail1(5).Tail0(7)
+	if b.BitString() != "000000011111" {
 		t.Error("FAIL")
 	}
 
-	b0 = NewBitString().Tail0(271)
-	if b0.String() != repeatRune('0', 271) {
+	b = NewBitString().Tail0(271)
+	if b.BitString() != repeatRune('0', 271) {
 		t.Error("FAIL")
 	}
 
-	b1 = NewBitString().Tail1(1490)
-	if b1.String() != repeatRune('1', 1490) {
+	b = NewBitString().Tail1(1490)
+	if b.BitString() != repeatRune('1', 1490) {
 		t.Error("FAIL")
 	}
+
+	b = Null()
+	if b.BitString() != "" {
+		t.Error("FAIL")
+	}
+
+	b = Ones(3)
+	if b.String() != "(3) 111" {
+		t.Error("FAIL")
+	}
+
+	b = Zeroes(7)
+	if b.String() != "(7) 0000000" {
+		t.Error("FAIL")
+	}
+
+	b = Zeroes(7).Tail1(4)
+	if b.String() != "(11) 11110000000" {
+		t.Error("FAIL")
+	}
+
+	b = Ones(7).Tail0(4)
+	if b.String() != "(11) 00001111111" {
+		t.Error("FAIL")
+	}
+
+	b, _ = FromString("11101")
+	if b.String() != "(5) 11101" {
+		t.Error("FAIL")
+	}
+
+	b, _ = FromString("011111")
+	if b.String() != "(6) 011111" {
+		t.Error("FAIL")
+	}
+
+	b = Random(2017)
+	a, _ = FromString(b.BitString())
+	if a.String() != b.String() {
+		t.Error("FAIL")
+	}
+
+	b = Random(1987)
+	a = b.Copy()
+	if a.String() != b.String() {
+		t.Error("FAIL")
+	}
+
+	/*
+
+
+	   x = bits.from_random(10)
+	   y = bits.from_random(10)
+	   z = x.copy().head(y)
+	   self.assertEqual(x.bit_str() + y.bit_str(), z.bit_str())
+
+
+	   x = bits.from_random(10)
+	   y = bits.from_random(10)
+	   z = x.copy().tail(y)
+	   self.assertEqual(y.bit_str() + x.bit_str(), z.bit_str())
+
+
+	   x = bits.from_tuple('11111111')
+	   y = x.get_bytes()
+	   self.assertEqual(len(y), 1)
+	   self.assertEqual(y[0], 255)
+
+
+	   x = bits.from_tuple('11011110101011011011111011101111')
+	   y = x.get_bytes()
+	   self.assertEqual(len(y), 4)
+	   self.assertEqual(y[0], 0xef)
+	   self.assertEqual(y[1], 0xbe)
+	   self.assertEqual(y[2], 0xad)
+	   self.assertEqual(y[3], 0xde)
+
+
+	   x = bits.bits().set_bytes((0xff,), 7)
+	   self.assertEqual(str(x), '(7) 1111111')
+	   x = bits.bits().set_bytes((0x1,), 7)
+	   self.assertEqual(str(x), '(7) 0000001')
+	   x = bits.bits().set_bytes((64,), 7)
+	   self.assertEqual(str(x), '(7) 1000000')
+
+
+	   str0 = '11011110101011011011111011101111'
+	   x = bits.from_tuple(str0)
+	   y = bits.bits().set_bytes(x.get_bytes(), len(str0))
+	   self.assertEqual(y.bit_str(), str0)
+
+
+	*/
 
 }
 
