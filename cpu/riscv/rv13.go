a7be48a9df7abbffafe68d35bb336ef53156817e
Jason Harris <sirmanlypowers@gmail.com>
2020-02-07 09:02:06 -0800

    Added rv13 debug registers.

--- cpu/riscv/rv13.go
+++ cpu/riscv/rv13.go
@@ -21,6 +21,41 @@ const irDmi = 0x11   // debug module interface access
 const drDtmcsLength = 32
 
 //-----------------------------------------------------------------------------
+// debug module debug bus registers
+
+const data0 = 0x04        // Abstract Data 0
+const data11 = 0x0f       // Abstract Data 11
+const dmcontrol = 0x10    // Debug Module Control
+const dmstatus = 0x11     // Debug Module Status
+const hartinfo = 0x12     // Hart Info
+const haltsum1 = 0x13     // Halt Summary 1
+const hawindowsel = 0x14  // Hart Array Window Select
+const hawindow = 0x15     // Hart Array Window
+const abstractcs = 0x16   // Abstract Control and Status
+const command = 0x17      // Abstract Command
+const abstractauto = 0x18 // Abstract Command Autoexec
+const confstrptr0 = 0x19  // Configuration String Pointer 0
+const confstrptr1 = 0x1a  // Configuration String Pointer 1
+const confstrptr2 = 0x1b  // Configuration String Pointer 2
+const confstrptr3 = 0x1c  // Configuration String Pointer 3
+const nextdm = 0x1d       // Next Debug Module
+const progbuf0 = 0x20     // Program Buffer 0
+const progbuf15 = 0x2f    // Program Buffer 15
+const authdata = 0x30     // Authentication Data
+const haltsum2 = 0x34     // Halt Summary 2
+const haltsum3 = 0x35     // Halt Summary 3
+const sbaddress3 = 0x37   // System Bus Address 127:96
+const sbcs = 0x38         // System Bus Access Control and Status
+const sbaddress0 = 0x39   // System Bus Address 31:0
+const sbaddress1 = 0x3a   // System Bus Address 63:32
+const sbaddress2 = 0x3b   // System Bus Address 95:64
+const sbdata0 = 0x3c      // System Bus Data 31:0
+const sbdata1 = 0x3d      // System Bus Data 63:32
+const sbdata2 = 0x3e      // System Bus Data 95:64
+const sbdata3 = 0x3f      // System Bus Data 127:96
+const haltsum0 = 0x40     // Halt Summary 0
+
+//-----------------------------------------------------------------------------
 
 type rv13 struct {
 	dev         *jtag.Device
